532528bdb63867ceacb12f86a05e55b1bad43fde
CRBP8B <CRBP8B@KC-SCE460LAB-20.kc.umkc.edu>
2016-02-16 12:53:35 -0600

    Assignment v1.1
    Bug fixes and correct implementation of the enum day of week

--- Assignment.h
+++ Assignment.h
@@ -7,11 +7,12 @@ using namespace std;
 
 class Assignment{
 
+private:
 	Date dueDate;
 	string description;
 	Date assignedDate;
-	//enum status { Assigned, Completed, Late };
-	//status stat;
+	enum status { assigned, completed, late };
+	status stat;
 
 public:
 
@@ -19,15 +20,43 @@ public:
 		dueDate = Date();
 		description = "";
 		assignedDate = Date();
-		//stat = status::Assigned;
+		stat = status::assigned;
 	}
 
-	Assignment(Date initDueDate, string initDesc, Date initAssigned, status initStat)
+	Assignment(Date initDueDate, string initDesc, Date initAssigned, string inputStat) //add status parameter.
 	{
 		dueDate = initDueDate;
 		description = initDesc;
-		assignedDate = initAssigned;
-		//stat = initStat;
+		assignedDate =initAssigned;
+
+		int casevar;
+		if (inputStat == "assigned")
+			casevar = 1;
+		else if (inputStat == "completed")
+			casevar = 2;
+		else if (inputStat == "late")
+			casevar = 3;
+		else
+		{
+			throw("That is not a valid status");
+			return;
+		}
+
+
+		switch (casevar)
+		{
+		case 1:
+			stat = assigned;
+			break;
+		case 2:
+			stat = completed;
+			break;
+		case 3:
+			stat = late;
+			break;
+		}
+
+
 	}
 
 	Date getDueDate()
@@ -59,7 +88,7 @@ public:
 	{
 		assignedDate = newDate;
 	}
-	/*
+	
 	status getStatus()
 	{
 		return stat;
@@ -69,7 +98,7 @@ public:
 	{
 		stat = newStat;
 	}
-	*/
+	
 };
 
 #endif
