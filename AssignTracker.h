54d330f28cc59bec208ec21d9e2af44b45df1024
CRBP8B <crbp8b@mail.umkc.edu>
2016-02-25 11:13:30 -0600

    Version 4.02 (Final Build)

--- AssignTracker.h
+++ AssignTracker.h
@@ -7,16 +7,17 @@
 #include <fstream>
 
 using namespace std;
+#ifndef ASSIGNTRACKER_H
+#define ASSIGNTRACKER_H
 
 class AssignTracker{
 
 private:
-	list<Assignment> assignedList;
+	list<Assignment> assignedList; //Keeps two lists, one ASSIGNED list, and one COMPLETED list. Late assignments go into the completed list
 	list<Assignment> completedList;
-	void keepOrdered();
-	void addCompleted(Assignment assign)
+	void addCompleted(Assignment assign) //This is a private function that adds an assignment directly to the COMPLETED list
 	{
-		assign.getAssignedDate().check_valid();//Check if date is valid
+		assign.getAssignedDate().check_valid();//Check if dates are valid
 		assign.getDueDate().check_valid();
 
 		if (assign.getDueDate() < assign.getAssignedDate())
@@ -32,31 +33,31 @@ private:
 
 		list<Assignment>::iterator it;
 
-		for (it = completedList.begin(); it != completedList.end(); ++it)
+		for (it = completedList.begin(); it != completedList.end(); ++it) //iterate through the list
 		{
-			if (assign.getDueDate() < it->getDueDate())
+			if (assign.getDueDate() < it->getDueDate()) //if our new assignment has found its place, break
 			{
 				break;
 			}
 		}
-		completedList.insert(it, assign);
+		completedList.insert(it, assign); //insert it using the built it inset function
 		return;
 
 	}
 
 public:
-	string printAssigned()
+	string printAssigned() //this returns a STRING of all assigned assignments in the list
 	{
 		ostringstream output;
-		for (list<Assignment>::iterator it = assignedList.begin(); it != assignedList.end(); ++it)
+		for (list<Assignment>::iterator it = assignedList.begin(); it != assignedList.end(); ++it) //iterate through the list
 		{
-			output << it->toString();
+			output << it->toString(); //send it into the ostringstream using the built in toString() function
 		}
 
 		return output.str();
 	}
 
-	string printCompleted()
+	string printCompleted() //this returns a STRING of all completed assignments in the list
 	{
 		ostringstream output;
 		for (list<Assignment>::iterator it = completedList.begin(); it != completedList.end(); ++it)
@@ -67,17 +68,17 @@ public:
 		return output.str();
 	}
 
-	bool addAssignment(Assignment newAssign)
+	bool addAssignment(Assignment newAssign) //this adds an assignment to either list, depending on the status
 	{
 		newAssign.getAssignedDate().check_valid();  //Check if date is valid
 		newAssign.getDueDate().check_valid();
 		
-		if (newAssign.getDueDate() < newAssign.getAssignedDate())
+		if (newAssign.getDueDate() < newAssign.getAssignedDate()) //if it
 		{
 			return false;
 		}
 			
-		if (newAssign.statusString() == "late" || newAssign.statusString() == "completed")
+		if (newAssign.statusString() == "late" || newAssign.statusString() == "completed") //if it's completed in any way, use the private addCompleted function
 		{
 			this->addCompleted(newAssign);
 			return true;
@@ -92,85 +93,55 @@ public:
 		
 		list<Assignment>::iterator it;
 
-		for (it = assignedList.begin(); it != assignedList.end(); ++it)
+		for (it = assignedList.begin(); it != assignedList.end(); ++it) //iterate through the list, find the correct position
 		{
 			if (newAssign.getDueDate() < it->getDueDate())
 			{
 				break;
 			}
 		}
-		assignedList.insert(it,newAssign);
+		assignedList.insert(it,newAssign); //insert it at that position
 		return true;
 	}
-/*	bool completeAssignment(Date assigned, Date completed)
-	{
-		list<Assignment>::iterator it = assignedList.begin();
-		
-		if (assignedList.empty()) //if the list is empty, return false
-			throw("The list is empty.");
 
-		
-		while (it->getAssignedDate() != assigned && it != assignedList.end())   //iterate through list, find the assignment, if possible
-		{
-			it++;   //Error <--------- const
-		}
-		
-
-		if (it == assignedList.end())  //if it cannot find the item, throw error
-			throw("Cannot find item");
-		Assignment toBeComp = *it;
-
-		if (completed > toBeComp.getDueDate())
-		{
-			toBeComp.setStatus("late");
-		}
-		else
-			toBeComp.setStatus("completed");
-
-		
-		addCompleted(toBeComp);      //add to completed list, and remove from the assigned list
-		assignedList.erase(it);
-		return true;
-
-	}*/
 	bool completeAssignment(Date assigned, Date completed)
 	{
-		assigned.check_valid();
+		assigned.check_valid(); //check to make sure the dates are valid
 		completed.check_valid();
 
-		if (assignedList.empty())
+		if (assignedList.empty()) //if the list is empty, it's obvious the assignment does not exist
 		{
 			return false;
 		}
 		list<Assignment>::iterator itr;
 
-		for (itr = assignedList.begin(); itr != assignedList.end(); ++itr)
+		for (itr = assignedList.begin(); itr != assignedList.end(); ++itr) //iterate through the list and find the assignment
 		{
 			if (itr->getAssignedDate() == assigned)
 				break;
 		}
 
-		if (itr == assignedList.end())
+		if (itr == assignedList.end()) //if it's the end, we didn't find it
 			return false;
 
 		Assignment toBeComp = *itr;
 
-		if (completed > toBeComp.getDueDate())
+		if (completed > toBeComp.getDueDate()) //set the correct status
 			toBeComp.setStatus("late");
 		else
 			toBeComp.setStatus("completed");
 
-		addCompleted(toBeComp);
-		assignedList.erase(itr);
+		addCompleted(toBeComp); //add to completedList
+		assignedList.erase(itr); //remove from assigned
 
 
 		return true;
 
 	}
 
-	bool editAssignment(Date assigned, Date newDue = Date(), string newDesc = "")
+	bool editAssignment(Date assigned, Date newDue = Date(), string newDesc = "") //takes in the assigned date, can take in newDueDate or newDescription or both. changes either or. returns true if nothing went wrong
 	{
-		assigned.check_valid();
+		assigned.check_valid(); //checks to see if the assignedDate and possibly DueDate are valid
 		if (newDue != Date())
 		{
 			newDue.check_valid();
@@ -184,32 +155,32 @@ public:
 
 		list<Assignment>::iterator itr;
 
-		for (itr = assignedList.begin(); itr != assignedList.end(); ++itr)
+		for (itr = assignedList.begin(); itr != assignedList.end(); ++itr) //find the assignment
 		{
 			if (itr->getAssignedDate() == assigned)
 				break;
 		}
 
-		if (itr == assignedList.end())
+		if (itr == assignedList.end()) //if we didn't find it, return false
 			return false;
 
 
-		if (newDue != Date())
+		if (newDue != Date()) //set the duedate, if necessary
 		{
 			itr->setDueDate(newDue);
 		}
 
 		if (newDesc != "")
 		{
-			itr->setDescription(newDesc);
+			itr->setDescription(newDesc); //set the desc, if necessary
 		}
 
 		return true;
 
 	}
-	bool readIn(string file)
+	bool readIn(string file) //reads the file with the name "file"  and reads the input into the correct list.
 	{
-		ifstream ifs;
+		ifstream ifs; //start the ifstream with the "file" as the name
 		ifs.open(file);
 
 		string line;
@@ -217,28 +188,26 @@ public:
 		string newStat, newDesc;
 		
 
-		while (ifs.good())
+		while (ifs.good()) //while there is more file to read
 		{
-			getline(ifs, line, '\n');
-			String_Tokenizer st(line, ",");
+			getline(ifs, line, '\n'); //read the next line in
+			String_Tokenizer st(line, ","); //create a string tokenizer 
 
-			while (st.has_more_tokens())
+			while (st.has_more_tokens()) //while loop to check if the tokenizer actually has tokens
 			{
-				dueInput = st.next_token();
-				newDesc = st.next_token();
-
-
-				newDesc = newDesc.substr(1);
+				dueInput = st.next_token(); //reaad in duedate
 				
+				newDesc = st.next_token(); //read in the desc, and trim off the leading space
+				newDesc = newDesc.substr(1);
 
-
-				assignedInput = st.next_token();
+				assignedInput = st.next_token(); //read in the assignedDate, and trim off leading space
 				assignedInput = assignedInput.substr(1);
-				newStat = st.next_token();
+
+				newStat = st.next_token();//read in the status, and trim off leading space
 				newStat = newStat.substr(1);
 
-				Assignment temp(Date(dueInput), newDesc, Date(assignedInput), newStat);
-				if (newStat == "late" || newStat == "completed")
+				Assignment temp(Date(dueInput), newDesc, Date(assignedInput), newStat); //create the assignment with the proper arguments
+				if (newStat == "late" || newStat == "completed") //add it to the appropiate list
 					this->addCompleted(temp);
 				else
 					this->addAssignment(temp);
@@ -248,12 +217,12 @@ public:
 		return true;
 	}
 
-	int countLate()
+	int countLate() //counts the amount of late assignments in line, and returns the amount. No variable is kept
 	{
 		int lateCount = 0;
 		list<Assignment>::iterator it;
 
-		for (it = completedList.begin(); it != completedList.end(); ++it)
+		for (it = completedList.begin(); it != completedList.end(); ++it) //iterate through the completed list, if it's late, increment the temp variable
 		{
 			if (it->statusString() == "late")
 			{
@@ -261,6 +230,7 @@ public:
 			}
 		}
 
-		return lateCount;
+		return lateCount; //return the value
 	}
 };
+#endif
--- Menu.h
+++ Menu.h
@@ -10,50 +10,49 @@ class Menu
 private:
 	AssignTracker tracker;
 public:
-	void printAll()
+	string printAll()
 	{
-		tracker.printAssigned();
-		tracker.printCompleted();
+		return (tracker.printAssigned() + tracker.printCompleted()); //calls the tracker's built in printAssigned and printCompleted functions, returns a STRING
 	}
-
-	bool addAssignment(Assignment assign)
+	
+	bool addAssignment(Assignment assign) //takes in an assignment, and uses the tracker's built in addAssignment() function
 	{
 		return tracker.addAssignment(assign);
 	}
 
-	bool editDueDate(string assignDate, string newDueDate)
+	bool editDueDate(string assignDate, string newDueDate) //Takes in the assignDate and a newDueDate. Uses the tracker's editAssignment.
 	{
 		return tracker.editAssignment(Date(assignDate), Date(newDueDate));
 	}
 
-	bool editDescription(string assignDate, string newDescription)
+	bool editDescription(string assignDate, string newDescription) //takes in the assignDate and the new Description. Uses the take editAssignment function, and uses a Date() as a placeholder
 	{
 		return tracker.editAssignment(Date(assignDate), Date() , newDescription);
 	}
 
-	bool completeAssignment(string assignDate, string completeDate)
+	bool completeAssignment(string assignDate, string completeDate) //takes in the assigndate for locating the assignment. Completes the assignment on the completeDate using the completeAssignment function of the tracker
 	{
 		return tracker.completeAssignment(Date(assignDate), Date(completeDate));
 	}
 
-	int numberOfLateAssignments()
+	int numberOfLateAssignments() //returns the int value of how many late assignments there are.
 	{
 		return tracker.countLate();
 	}
 
-	string printNumberLate()
+	string printNumberLate() //uses the above function to output a string that states how many functions were late.
 	{
-		return ("There are " + to_string(numberOfLateAssignments()) + "late assignments\n");
+		return ("There are " + to_string(numberOfLateAssignments()) + " late assignments\n");
 	}
 
-	void saveToFile(string outputFile = "output.txt")
+	void saveToFile(string outputFile = "output.txt") //Uses an output file (default = output.txt) to write to, using the printfunctions.
 	{
 		ofstream fout(outputFile);
 		fout << tracker.printAssigned() << tracker.printCompleted();
 		fout.close();
 	}
 
-	bool readFileIn(string inputFile)
+	bool readFileIn(string inputFile) //uses the readIn function to read in assignments from a inputFile.
 	{
 		return tracker.readIn(inputFile);
 	}
